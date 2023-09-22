b9a5ce9927b15ec7f1327755d1735a62bdbbdb14
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-09-22 15:08:21 +0200

    AJout

---
+++ MatchingGame_VictorPholien/MatchingGame_VictorPholien/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchingGame_VictorPholien
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+                {
+                "🐈","🐈",
+                "🐷","🐷",
+                "🐐","🐐",
+                "🦊","🦊",
+                "🐴","🐴",
+                "🦨","🦨",
+                "🦉","🦉",
+                "🐀","🐀",
+                };
+            foreach (TextBlock textBlock in grdMain.Children.OfType<TextBlock>())
+            {
+                int index = nbAlea.Next(animalEmoji.Count); // index est de type int
+                                                        // nbalea est un objet de type Random()
+                string nextEmoji = animalEmoji[index]; // nextEmoji est de type string
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index); // on retire un animal de la liste pour ne pas l’attribuer à nouveau.
+            }
+
+
+        }
+    }
+}
